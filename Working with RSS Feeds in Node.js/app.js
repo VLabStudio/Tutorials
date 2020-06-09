d372df2c9a4e08f2e63d02015950a25171788ba9
Vincent Drews <accounts@vincentlab.net>
2020-06-09 12:50:52 +0200

    Fixed the code in Working with RSS Feeds in Node.js

--- Working with RSS Feeds in Node.js/app.js
+++ Working with RSS Feeds in Node.js/app.js
@@ -14,15 +14,16 @@ const Parser = require("rss-parser");
 
     // Clean up the string and replace reserved characters
     const fileName = `${feed.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
+
     if (fs.existsSync(fileName)) {
-        items = require(fileName);
+        items = require(`./${fileName}`);
     }
 
     // Add the items to the items array
     await Promise.all(feed.items.map(async (currentItem) => {
 
         // Add a new item if it doesn't already exist
-        if (items.filter((item) => item === currentItem).length <= 1) {
+        if (items.filter((item) => isEquivalent(item, currentItem)).length <= 0) {
             items.push(currentItem);
         }
 
@@ -32,3 +33,26 @@ const Parser = require("rss-parser");
     fs.writeFileSync(fileName, JSON.stringify(items));
 
 })();
+
+function isEquivalent(a, b) {
+    // Create arrays of property names
+    let aProps = Object.getOwnPropertyNames(a);
+    let bProps = Object.getOwnPropertyNames(b);
+
+    // if number of properties is different, objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (let i = 0; i < aProps.length; i++) {
+        let propName = aProps[i];
+
+        // if values of same property are not equal, objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // if we made it this far, objects are considered equivalent
+    return true;
+}
